cd43e05c4b4e8f2772c0a0fda0b182db75354377
Kyle-corbeel <37397551+Kyle-corbeel@users.noreply.github.com>
2018-03-20 22:16:50 +0100

    personalisering

--- GameOfLife/src/gameoflife/Personalisering.java
+++ GameOfLife/src/gameoflife/Personalisering.java
@@ -10,5 +10,46 @@ package gameoflife;
  * @author kyle-
  */
 public class Personalisering {
+    private int[] kleurAchtergrond;
+    private int[] kleurDood;
+    private int[] kleurLevend;
+    
+    public Personalisering()
+    {
+        int[] kleurAchtergrond = {125,125,125};
+        int[] kleurDood = {255,255,255};
+        int[] kleurLevend = {0,0,0};
+        
+    }
+    
+    /**
+     * Verander de kleur van levende cellen
+     * @param kleur gekozen kleur
+     */
+    
+    public void kiesKleurLevend(int[] kleur)
+    {
+        kleurLevend = kleur;
+    }
+    
+    /**
+     * Verander de kleur van dode  cellen
+     * @param kleur  gekozen kleur
+     */
+    
+    public void kiesKleurDood(int[] kleur)
+    {
+        kleurDood = kleur;
+    }
+    
+    /**
+     * Verand de kleur van de achtergrodn
+     * @param kleur de gekozen kleur
+     */
+    
+    public void kiesKleurAchtergrond(int[] kleur)
+    {
+        kleurAchtergrond = kleur;
+    }
     
 }
